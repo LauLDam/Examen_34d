849acaddfd644e8c7e3d5f35bac852b14f3f2aba
LauraVillar <damj15.2014@gmail.com>
2024-06-13 09:50:04 +0200

    Proyecto documentado

--- src/examen/IPValidator.java
+++ src/examen/IPValidator.java
@@ -1,9 +1,28 @@
 package examen;
 
 public class IPValidator {
+	
+	/**
+	 * Esta clase valida la letra (el tipo) de la IP con la dirección IP
+	 * 
+	 * autor Laura Villar
+	 * 
+	 * version 1.0
+	 * 
+	 */
 	public static String ipPattern;
+	
+	/**
+	 * Método para validar si una IP coincide con el rango numérico correcto, se le asigna el tipo (la letra) que le corresponde
+	 * 
+	 * @param ip El parámetro de la dirección IP
+	 * @param ipClass El parámetro de la letra de la IP 
+	 * @return la IP 
+	 */
 
 	public static boolean validateIPAndClass(String ip, char ipClass) {
+	
+		
 		boolean validIPClass = true;
 		ipPattern = "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
 				+ "\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
